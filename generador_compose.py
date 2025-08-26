ca877f4fd75d84e4f35863734cd73d8ea44e425b
Facundolescano0 <faculescano00@gmail.com>
2025-08-25 23:27:08 -0300

    fix error de ruta en docker compose

--- generador_compose.py
+++ generador_compose.py
@@ -14,7 +14,7 @@ class GeneradorCompose:
         f.write("    networks:\n")
         f.write("      - testing_net\n")
         f.write("    volumes:\n")
-        f.write("      - ./server/config.ini:/app/config.ini\n")
+        f.write("      - ./server/config.ini:/config.ini\n")
         f.write("\n")
 
     def generar_clientes(self, f, cantidad_clientes):
@@ -32,7 +32,7 @@ class GeneradorCompose:
             f.write(f"    depends_on:\n")
             f.write(f"      - server\n")
             f.write(f"    volumes:\n")
-            f.write(f"      - ./client/config.yaml:/app/config.yaml\n")
+            f.write(f"      - ./client/config.yaml:/config.yaml\n")
             f.write("\n")
 
     def generar_servicios(self, f, cantidad_clientes):
